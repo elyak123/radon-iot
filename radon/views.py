b77aba07a31787d97e99b251dffa2d20f2c60032
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-01-31 15:44:33 -0600

    funcion de lectura regresa valores

--- radon/views.py
+++ radon/views.py
@@ -2,10 +2,11 @@ def lectura(event, context):
     # Tenemos que guardar en RDS (Postgres).
     # Tenemos que regresar un OK por cualquier cuestión.
     # Aqui hacer la separación.
-    return "Hola desde Lectura"
+    return "Hola desde Lectura\n{}\n{}".format(str(event), str(context))
 
-def decode_float_little_endian(hex_val):
-    return struct.unpack("<f", bytes.fromhex(hex_val))[0]
 
-def decode_int_little_endian(hex_val):
-    return struct.unpack("<i", bytes.fromhex(hex_val+'0000'))[0]
+# def decode_float_little_endian(hex_val):
+#     return struct.unpack("<f", bytes.fromhex(hex_val))[0]
+
+# def decode_int_little_endian(hex_val):
+#     return struct.unpack("<i", bytes.fromhex(hex_val+'0000'))[0]
