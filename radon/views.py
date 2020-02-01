40c906ce6aa72333f2a0b3f81b4097f9879537f4
Javier Llamas Ramirez <jllamas@insecami.com.mx>
2020-02-01 13:43:42 -0600

    se agrega evento en SNS y primer esbozo de la estructura del evento dentro de lambda

--- radon/views.py
+++ radon/views.py
@@ -1,12 +1,20 @@
+import struct
+
+
 def lectura(event, context):
     # Tenemos que guardar en RDS (Postgres).
     # Tenemos que regresar un OK por cualquier cuestión.
     # Aqui hacer la separación.
-    return "Hola desde Lectura\n{}\n{}".format(str(event), str(context))
+    angulo = decode_int_little_endian(event['Records']['Sns']['Message'][0:4])
+    temperatura = decode_float_little_endian(event['Records']['Sns']['Message'][4:12])
+    humedad = decode_float_little_endian(event['Records']['Sns']['Message'][4:12])
+    return "Hola desde Lectura\n{}\n{}\nangulo:{}\ntemperatura:{}\nhumedad:{}".format(
+        str(event), str(context), angulo, temperatura, humedad)
 
 
-# def decode_float_little_endian(hex_val):
-#     return struct.unpack("<f", bytes.fromhex(hex_val))[0]
+def decode_float_little_endian(hex_val):
+    return struct.unpack("<f", bytes.fromhex(hex_val))[0]
 
-# def decode_int_little_endian(hex_val):
-#     return struct.unpack("<i", bytes.fromhex(hex_val+'0000'))[0]
+
+def decode_int_little_endian(hex_val):
+    return struct.unpack("<i", bytes.fromhex(hex_val+'0000'))[0]
