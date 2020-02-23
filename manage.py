442db7ab2b2f76c120f7dc0be51ee67097ba406e
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-02-23 16:42:01 -0600

    reestructura completa

--- manage.py
+++ manage.py
@@ -2,10 +2,34 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import environ
+
+
+def settings_module_handler():
+    ROOT_DIR = environ.Path(__file__) - 1
+    env = environ.Env()
+    # .env file, should load only in development environment
+    READ_DOT_ENV_FILE = env.bool('DJANGO_READ_DOT_ENV_FILE', default=False)
+
+    if __name__ == "__main__":
+        if READ_DOT_ENV_FILE:
+            # Operating System Environment variables have precedence over variables defined in the .env file,
+            # that is to say variables from the .env files will only be used if not defined
+            # as environment variables.
+            env_file = str(ROOT_DIR.path('.env'))
+            env.read_env(env_file)
+            os.environ.setdefault(
+                "DJANGO_SETTINGS_MODULE",
+                env('DJANGO_SETTINGS_MODULE')
+            )
+        else:
+            os.environ.setdefault(
+                "DJANGO_SETTINGS_MODULE", "radon.config.settings.production"
+            )
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'radon.config.settings.settings')
+    settings_module_handler()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
