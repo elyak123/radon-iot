aae2fd2c33f3b6da5a4c7b7c6dd853e6b96048f6
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-03-02 16:51:30 -0600

    se agrega instruccion para confirmar subscripcion

--- radon/iot/views.py
+++ radon/iot/views.py
@@ -1,14 +1,21 @@
+import json
+import requests
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+#from .captura import lectura
 
 @csrf_exempt
 def lectura(request):
     if request.method == 'POST':
-        print(request)
-        print(request.POST)
-        print(request.body)
-        print(request.headers)
+        try:
+            body = json.loads(request.body)
+        except json.decoder.JSONDecodeError:
+            body = request.body
+        print(body)
+        if body['Type'] == 'SubscriptionConfirmation':
+            requests.get(body['SubscribeURL'])
+        #mensaje = lectura(body['Message'])
         return HttpResponse('post')
     else:
         return HttpResponse('get')
