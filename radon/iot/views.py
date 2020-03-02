6e356f230d14f02771297c706628a66841a2f03e
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-03-02 14:29:04 -0600

    se agrega endpoint para AWS SNS

--- radon/iot/views.py
+++ radon/iot/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
-# Create your views here.
+@csrf_exempt
+def lectura(request):
+    if request.method == 'POST':
+        print(request)
+        print(request.POST)
+        print(request.body)
+        print(request.headers)
+        return HttpResponse('post')
+    else:
+        return HttpResponse('get')
