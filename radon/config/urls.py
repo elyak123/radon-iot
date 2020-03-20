66889edfac84d82a17f8b88c1efa48af24a38271
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-03-20 17:23:29 -0600

    se agregan serializadores y comienzan vistas rest

--- radon/config/urls.py
+++ radon/config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.iot.urls')),
     path('iot/', include('radon.iot.urls')),
 ]
