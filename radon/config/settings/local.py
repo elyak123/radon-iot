c234962f8a07aeb8389407e333aaeed669c22c7c
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-04-15 07:31:04 -0500

    settings para requests de prueba

--- radon/config/settings/local.py
+++ radon/config/settings/local.py
@@ -15,3 +15,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 # Note: This key only used for development and testing.
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='-r&khdeo$%xzj!o_xj@4ry=u7$urt(n@m8u@1ive#r46i@i5cs')
+TEST_USERNAME = env('TEST_USERNAME', default='')
+TEST_PASSWORD = env('TEST_PASSWORD', default='')
+TEST_URL = env('TEST_URL', default='')
