66bfaad4093a273908ae0e1ab74b383842b4c1d1
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-04-16 22:56:54 -0500

    Se agrega metodo send para otras formas de envios http

--- radon/users/client.py
+++ radon/users/client.py
@@ -54,21 +54,23 @@ class RadonCookieRequests(object):
             self.refresh_or_login()
         return URL, headers
 
-    def post(self, url_specific, payload, specific_headers={}):
+    def send(self, method, url_specific, payload={}, specific_headers={}):
         URL, headers = self.prepare_request(url_specific, specific_headers)
-        response = self.session.post(URL, headers=headers, json=payload)
+        handler = getattr(self.session, method.lower())
+        if method in ['post', 'put', 'patch']:
+            response = handler(URL, headers=headers, json=payload)
+        else:
+            response = handler(URL, headers=headers)
         try:
             return response.json()
         except json.decoder.JSONDecodeError:
             return response.status_code
 
+    def post(self, url_specific, payload, specific_headers={}):
+        return self.send('post', url_specific, payload, specific_headers)
+
     def get(self, url, specific_headers={}):
-        URL, headers = self.prepare_request(url, specific_headers)
-        response = self.session.get(URL, headers=headers)
-        try:
-            return response.json()
-        except json.decoder.JSONDecodeError:
-            return response.status_code
+        return self.send('get', url, specific_headers)
 
 
 client = RadonCookieRequests()
