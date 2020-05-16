31805768cd32069b6dcd448bf3d707b61f4d3f10
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-05-16 10:52:23 -0500

    factories para iot incluyendo ubicacion de dispositivos

--- radon/users/tests/factories.py
+++ radon/users/tests/factories.py
@@ -5,11 +5,11 @@ from django.contrib.auth import get_user_model
 from radon.users.models import Gasera
 
 User = get_user_model()
-faker = Faker(['es_MX'])
+fake = Faker(['es_MX'])
 
 
 class GaseraFactory(factory.django.DjangoModelFactory):
-    nombre = factory.LazyAttribute(lambda o: '{} {}'.format(faker.company(), faker.company_suffix()))
+    nombre = factory.LazyAttribute(lambda o: '{} {}'.format(fake.company(), fake.company_suffix()))
 
     class Meta:
         model = Gasera
@@ -18,9 +18,9 @@ class GaseraFactory(factory.django.DjangoModelFactory):
 
 class UserFactory(factory.django.DjangoModelFactory):
     username = factory.LazyAttribute(lambda o: o.first_name.split(' ')[0].lower())
-    first_name = faker.first_name()
-    last_name = faker.last_name()
-    email = factory.LazyAttribute(lambda o: '{}@{}'.format(o.username, faker.domain_name()))
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    email = factory.LazyAttribute(lambda o: '{}@{}'.format(o.username, fake.domain_name()))
     password = factory.PostGenerationMethodCall('set_password', 'password')
     gasera = factory.SubFactory(GaseraFactory)
     tipo = FuzzyChoice(['CLIENTE', 'CONSUMIDOR', 'STAFF', 'OPERARIO'])
