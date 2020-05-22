83d52834b3b3e015e10ec2021681356207f61a92
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-05-22 11:16:02 -0500

    se incluyen rutas a poblar

--- radon/iot/management/commands/poblar.py
+++ radon/iot/management/commands/poblar.py
@@ -1,4 +1,5 @@
 import pytz
+import datetime
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.core.exceptions import ImproperlyConfigured
@@ -16,13 +17,15 @@ class Command(BaseCommand):
             call_command('flush')
             cantidad_gaseras = 10
             consumidores_por_gasera = 18
-            pedidos_por_usuario = 20
             vechiculos_por_gasera = 12
-            rutas_por_gasera = (pedidos_por_usuario * consumidores_por_gasera) / 30
+            jornadas_por_gasera = 30
+            pedidos_por_usuario = int((vechiculos_por_gasera * jornadas_por_gasera) / consumidores_por_gasera)
             tz = pytz.timezone(settings.TIME_ZONE)
             gaseras = user_factories.GaseraFactory.create_batch(cantidad_gaseras)
             for index, gasera in enumerate(gaseras):
-                usrs = user_factories.UserFactory.create_batch(consumidores_por_gasera, gasera=gasera, tipo='CONSUMIDOR')
+                usrs = user_factories.UserFactory.create_batch(
+                    consumidores_por_gasera, gasera=gasera, tipo='CONSUMIDOR'
+                )
                 usuario_cliente = user_factories.UserFactory(gasera=gasera, tipo='CLIENTE')
                 self.stdout.write(self.style.SUCCESS(
                     f'Usuario consumidor: {usrs[0].username}\n'
@@ -33,6 +36,14 @@ class Command(BaseCommand):
                 usrs[0].set_password('password')
                 dt = iot_factories.DeviceTypeFactory()
                 ws = iot_factories.WisolFactory.create_batch(consumidores_por_gasera, deviceTypeId=dt)
+                vehiculos = rutas_factories.VehiculoFactory.create_batch(vechiculos_por_gasera, gasera=gasera)
+                today = datetime.datetime.now()
+                for day in range(jornadas_por_gasera):
+                    fecha = today - datetime.timedelta(days=-day)
+                    jornada = rutas_factories.JornadaFactory(gasera=gasera, fecha=fecha)
+                    for vehiculo in vehiculos:
+                        ruta = rutas_factories.RutaFactory(jornada=jornada)
+                        ruta.vehiculo.add(vehiculo)
                 for usr, wisol in zip(usrs, ws):
                     disp = iot_factories.DispositivoFactory(wisol=wisol, usuario=usr)
                     pds = rutas_factories.PedidoFactory.create_batch(pedidos_por_usuario, dispositivo=disp)
@@ -40,6 +51,5 @@ class Command(BaseCommand):
                         Pedido.objects.filter(pk=pedido.pk).update(
                             fecha_creacion=rutas_factories.fake.date_time_this_month(tzinfo=tz)
                         )
-                rutas_factories.VehiculoFactory.create_batch(vechiculos_por_gasera, gasera=gasera)
         else:
             raise ImproperlyConfigured('No tienes settings.DEBUG activado, la operación no se puede completar.')
