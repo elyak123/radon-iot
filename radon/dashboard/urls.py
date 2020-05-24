0be8d5aefac0ebbdcf7f03782abcb3bbe284980c
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-05-24 17:10:20 -0500

    pequenos cambios esteticos a urls de dashboard

--- radon/dashboard/urls.py
+++ radon/dashboard/urls.py
@@ -6,9 +6,9 @@ app_name = 'dashboard'
 urlpatterns = [
     path(r'', views.DashboardView.as_view(), name='inicio'),
     path(r'dispositivos/', views.DispositivoListView.as_view(), name='dispositivo_list'),
-    path(r'dispositivos/<pk>', views.DispositivoDetailView.as_view(), name='dispositivo_detail'),
-    path(r'dispositivos/eliminar/<pk>', views.DispositivoDeleteView.as_view(), name='dispositivo_delete'),
-    path(r'dispositivos/editar/<pk>', views.DispositivoUpdateView.as_view(), name='dispositivo_update'),
+    path(r'dispositivos/<int:pk>/', views.DispositivoDetailView.as_view(), name='dispositivo_detail'),
+    path(r'dispositivos/eliminar/<int:pk>/', views.DispositivoDeleteView.as_view(), name='dispositivo_delete'),
+    path(r'dispositivos/editar/<int:pk>/', views.DispositivoUpdateView.as_view(), name='dispositivo_update'),
     re_path(r'^pedidos/(?:(?P<week>\d+)/)?$', views.PedidoView.as_view(), name="histograma_pedidos"),
-    path(r'pedidos/nuevo', views.PedidoCreateView.as_view(), name='pedido_creation')
+    path(r'pedidos/nuevo/', views.PedidoCreateView.as_view(), name='pedido_creation')
 ]
