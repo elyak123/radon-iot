55bb707f670b06d79eeabb9822fc68cfc4f0ac20
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-05-27 13:33:47 -0500

    se corrige CONDITIONAL_REQUIRED_FIELDS

--- radon/rutas/models.py
+++ radon/rutas/models.py
@@ -160,7 +160,7 @@ class Pedido(ModelFieldRequiredMixin, models.Model):
 
     CONDITIONAL_REQUIRED_FIELDS = [
         (
-            lambda instance: actualizado, ['orden'],
+            lambda instance: instance.actualizado, ['orden'],
         ),
     ]
 
