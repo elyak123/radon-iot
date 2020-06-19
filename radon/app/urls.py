7aec07b9c6d6d0aef17c0836f7ac9da3d7aadad6
jllamas <jllamas@insecami.com.mx>
2020-06-19 12:07:27 -0500

    modificacion a la pwa de consumidor

--- radon/app/urls.py
+++ radon/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from radon.app.views import DashboardView
 
 urlpatterns = [
+    path('', DashboardView.as_view(), name="inicio"),
     path('', include('pwa.urls')),  # You MUST use an empty string as the URL prefix
-    path('inicio/', DashboardView.as_view(), name="inicio")
 ]
