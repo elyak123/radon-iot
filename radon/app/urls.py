e8913764fa84addde61a154d9270d587b75965b4
jllamas <jllamas@insecami.com.mx>
2020-07-09 12:28:52 -0500

    endpoint de sigfox

--- radon/app/urls.py
+++ radon/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from radon.app.views import DashboardView
+from radon.app.views import DashboardView, sigfox
 
 urlpatterns = [
     path('', DashboardView.as_view(), name="inicio"),
+    path('sigfox/', sigfox),
     path('', include('pwa.urls')),  # You MUST use an empty string as the URL prefix
 ]
--- radon/app/views.py
+++ radon/app/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from radon.users.auth import AuthenticationTestMixin
 from django.views import generic
+from rest_framework.response import Response
+from rest_framework import status
+from radon.users.auth import AuthenticationTestMixin
 
-# Create your views here.
+
 class DashboardView(generic.TemplateView):
     template_name = "app/inicio.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['ultima_lectura'] = {'lectura': self.request.user.dispositivo_set.first().get_ultima_lectura()}
+        return context
+
+
+def sigfox(request):
+    return Response({}, status.HTTP_204_NO_CONTENT)
