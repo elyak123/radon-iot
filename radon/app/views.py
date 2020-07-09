aa33c5048beb28c59fa5ea0893f8c7e7113b99a9
jllamas <jllamas@insecami.com.mx>
2020-07-09 12:50:32 -0500

    modificacion de vistas

--- radon/app/views.py
+++ radon/app/views.py
@@ -1,20 +1,19 @@
 from django.views import generic
-from rest_framework.response import Response
-from rest_framework import status
+from django.http import HttpResponse
 from radon.users.auth import AuthenticationTestMixin
 
 
 class DashboardView(generic.TemplateView):
     template_name = "app/inicio.html"
 
-    def get_context_data(self, **kwargs):
-        context = super(DashboardView, self).get_context_data(**kwargs)
-        context['ultima_lectura'] = {'lectura': self.request.user.dispositivo_set.first().get_ultima_lectura()}
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super(DashboardView, self).get_context_data(**kwargs)
+    #     context['ultima_lectura'] = {'lectura': self.request.user.dispositivo_set.first().get_ultima_lectura()}
+    #     return context
 
 
 def sigfox(request):
-    return Response({}, status.HTTP_204_NO_CONTENT)
+    return HttpResponse(status=204)
 
 
 class GraphView(generic.TemplateView):
