df10686eb3e28381dbcdfcb148414c87fa6d4fbb
jllamas <jllamas@insecami.com.mx>
2020-07-14 15:53:38 -0500

    cambios al wsgi.py para soportar variable de entorno con .env

--- radon/config/wsgi.py
+++ radon/config/wsgi.py
@@ -8,9 +8,28 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import environ
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'radon.settings')
+env = environ.Env()
+READ_DOT_ENV_FILE = env.bool('DJANGO_READ_DOT_ENV_FILE', default=False)
+WSGI_PATH = environ.Path(__file__)  # radon/radon/config/wsgi.py
+ROOT_DIR = WSGI_PATH - 3  # radon/radon/config/wsgi.py -3 = radon/
+
+if READ_DOT_ENV_FILE:
+    # Operating System Environment variables have precedence over variables defined in the .env file,
+    # that is to say variables from the .env files will only be used if not defined
+    # as environment variables.
+    env_file = str(ROOT_DIR.path('.env'))
+    env.read_env(env_file)
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        env('DJANGO_SETTINGS_MODULE')
+    )
+else:
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "radon.config.settings.production"
+    )
 
 application = get_wsgi_application()
