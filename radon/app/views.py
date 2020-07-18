64770f6967d30a841aa816cc6d2f2861c6472cbb
jllamas <jllamas@insecami.com.mx>
2020-07-18 14:39:47 -0500

    reparacion de vistas para app

--- radon/app/views.py
+++ radon/app/views.py
@@ -5,10 +5,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class DashboardView(LoginRequiredMixin, generic.TemplateView):
     template_name = "app/inicio.html"
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(DashboardView, self).get_context_data(**kwargs)
-    #     context['ultima_lectura'] = {'lectura': self.request.user.dispositivo_set.first().get_ultima_lectura()}
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['ultima_lectura'] = {'lectura': self.request.user.dispositivo_set.first().get_ultima_lectura()}
+        return context
 
 
 class GraphView(generic.TemplateView):
