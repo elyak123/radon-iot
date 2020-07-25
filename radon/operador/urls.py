4726fa35da75a0f1760bc7573a3f40ba9e5e5406
Carlos Daniel Cañedo García <carlosdany58@hotmail.com>
2020-07-25 14:05:46 -0500

    Cambios de interfaz

--- radon/operador/urls.py
+++ radon/operador/urls.py
@@ -4,6 +4,7 @@ from radon.operador.views import DashboardView
 
 urlpatterns = [
     path('', DashboardView.as_view(), name="inicio"),
+    path('creacion-usuario', CreacionUsuarioView.as_view(), name="creacion-usuario"),
     path('', include('pwa.urls')),  # You MUST use an empty string as the URL prefix
     path('', include('radon.accounts.urls')),
 ]
--- radon/operador/views.py
+++ radon/operador/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class DashboardView(LoginRequiredMixin, generic.TemplateView):
     template_name = "operador/inicio.html"
+
+
+class CreacionUsuarioView(LoginRequiredMixin, generic.FormView):
+    template_name = "operador/creacion-usuario.html"
+
