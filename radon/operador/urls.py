c3b350eebd7ced9fcff70edc42958f1f8f74d29a
Carlos Daniel Cañedo García <carlosdany58@hotmail.com>
2020-08-01 14:04:37 -0500

    LIgeros cambios

--- radon/operador/urls.py
+++ radon/operador/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from radon.operador import views
 
 
 urlpatterns = [
     path('', views.DashboardView.as_view(), name="inicio"),
-    path('creacion-usuario', views.CreacionUsuarioView.as_view(), name="creacion-usuario"),
+    path('creacion-usuario/', views.CreacionUsuarioView.as_view(), name="creacion-usuario"),
+    path('checar-email/', views.ChecarEmailView, name="checar-email"),
     path('', include('pwa.urls')),  # You MUST use an empty string as the URL prefix
     path('', include('radon.accounts.urls')),
+    re_path(r'^auth/', include('dj_rest_auth.urls'))
 ]
--- radon/operador/views.py
+++ radon/operador/views.py
@@ -1,6 +1,10 @@
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from radon.iot.models import Instalacion
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import permissions, status
+from radon.users import serializers
 
 
 class DashboardView(LoginRequiredMixin, generic.ListView):
@@ -12,5 +16,14 @@ class DashboardView(LoginRequiredMixin, generic.ListView):
         return self.model.objects.filter(operario=self.request.user).order_by('-fecha')
 
 
-class CreacionUsuarioView(LoginRequiredMixin, generic.FormView):
+class CreacionUsuarioView(LoginRequiredMixin, generic.TemplateView):
     template_name = "operador/creacion-usuario.html"
+
+
+@api_view(['POST'])
+@permission_classes(permissions.AllowAny)
+def ChecarEmailView(request):
+    serializer = serializers.EmailValidator(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status.HTTP_200_OK, {})
