c19d24b3865958082b72144ade1a1adcaf5e62d8
Carlos Daniel Cañedo García <carlosdany58@hotmail.com>
2020-08-07 12:57:39 -0500

    Se avanza en el registro de usuarios

--- radon/operador/urls.py
+++ radon/operador/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', include('pwa.urls')),  # You MUST use an empty string as the URL prefix
     path('', include('radon.accounts.urls')),
     path('iot/', include('radon.iot.urls')),
+    path('users/', include('radon.users.urls')),
     re_path(r'^auth/login/$', UsersLoginView.as_view(), name='rest_login'),
     re_path(r'^auth/refresh/$', RefreshUsersView.as_view(), name='token_refresh'),
     re_path(r'^auth/', include('dj_rest_auth.urls')),
