5dfe441c81beb2e2da207b564e5ed8262ca4d985
jllamas <jllamas@insecami.com.mx>
2020-08-10 13:08:12 -0500

    quitamos celery

--- radon/config/settings/production.py
+++ radon/config/settings/production.py
@@ -19,7 +19,6 @@ from .base import *  # noqa
 import re
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-from sentry_sdk.integrations.celery import CeleryIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
 
 # SECRET CONFIGURATION
