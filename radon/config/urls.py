6f9edc77cb42dd24b61241e55e5ecc1dc1f5261f
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-08-28 10:50:14 -0500

    conf url se convierte para admin

--- radon/config/urls.py
+++ radon/config/urls.py
@@ -22,17 +22,18 @@ from radon.users.views import UsersLoginView, RefreshUsersView
 
 
 urlpatterns = [
-    path('', include('radon.dashboard.urls')),
-    re_path(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-            TemplateView.as_view(template_name="account/password_reset_confirm.html"),
-            name='password_reset_confirm'),
-    re_path(r'^auth/login/$', UsersLoginView.as_view(), name='rest_login'),
-    re_path(r'^auth/refresh/$', RefreshUsersView.as_view(), name='token_refresh'),
-    re_path(r'^auth/', include('dj_rest_auth.urls')),
-    path('admin/', admin.site.urls),
-    path('usuarios/', include('radon.accounts.urls')),
-    path('usuarios/', include('radon.users.urls')),
-    path('iot/', include('radon.iot.urls')),
+    path('', admin.site.urls),
+    # path('', include('radon.dashboard.urls')),
+    # re_path(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #         TemplateView.as_view(template_name="account/password_reset_confirm.html"),
+    #         name='password_reset_confirm'),
+    # re_path(r'^auth/login/$', UsersLoginView.as_view(), name='rest_login'),
+    # re_path(r'^auth/refresh/$', RefreshUsersView.as_view(), name='token_refresh'),
+    # re_path(r'^auth/', include('dj_rest_auth.urls')),
+    # path('admin/', admin.site.urls),
+    # path('usuarios/', include('radon.accounts.urls')),
+    # path('usuarios/', include('radon.users.urls')),
+    # path('iot/', include('radon.iot.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += [
