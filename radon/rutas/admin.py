b05d546bbe3eb155e742e084de7a76f2f414102a
Carlos Daniel Cañedo García <carlosdany58@hotmail.com>
2020-09-04 12:53:57 -0500

    LIgeros cambios al template offline, se añaden modelos a admin

--- radon/rutas/admin.py
+++ radon/rutas/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Pedido
 
 # Register your models here.
+
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'telefono', 'dispositivo', 'cantidad', 'jornada', 'gasera')
+    search_fields = ("dispositivo__usuario__gasera__nombre__icontains", 'dispositivo__usuario__username__icontains', 'dispositivo__wisol__serie__icontains')
+
+    def gasera(self, obj):
+        return obj.dispositivo.usuario.gasera
+
+    def usuario(self, obj):
+        return obj.dispositivo.usuario
+
+    def telefono(self, obj):
+        return format_html('<a href="tel:{}">{}</a>', obj.dispositivo.usuario.telefono, obj.dispositivo.usuario.telefono)
+
+
+admin.site.register(Pedido, PedidoAdmin)
