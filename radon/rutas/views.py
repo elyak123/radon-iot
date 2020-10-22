a7f3f6df87a1c1c810a3bb7b647973b68b557e1e
jllamas <jllamas@insecami.com.mx>
2020-10-22 12:35:38 -0500

    se corrige error que no regresa httpresponse

--- radon/rutas/views.py
+++ radon/rutas/views.py
@@ -3,6 +3,5 @@ from django.http import JsonResponse
 
 def endpointgps(request):
     if request.method == 'GET':
-        JsonResponse({'metodo': 'GET', 'contenido': 'Hola desde GET'})
-    elif request.method == 'POST':
-        JsonResponse({'metodo': 'POST', 'contenido': 'Hola desde POST'})
+        return JsonResponse({'metodo': 'GET', 'contenido': 'Hola desde GET'})
+    return JsonResponse({'metodo': 'POST', 'contenido': 'Hola desde POST'})
