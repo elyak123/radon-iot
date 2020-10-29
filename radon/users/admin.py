5ee372074e4543739247481878f27f1589774366
jllamas <jllamas@insecami.com.mx>
2020-10-29 14:52:04 -0600

    se modifica el formulario de creacion de usuarios para admin

--- radon/users/admin.py
+++ radon/users/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-
-class UserAdmin(admin.ModelAdmin):
-    pass
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .forms import UserForm
 
 
+class UserAdmin(UserAdmin):
+    add_form = UserForm
+    prepopulated_fields = {'username': ('first_name' , 'last_name', )}
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'last_name', 'username', 'password1', 'password2', ),
+        }),
+    )
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
