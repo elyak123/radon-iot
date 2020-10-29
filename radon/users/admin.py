56b416f09b3a343d4d56c1493da6a4609f56fc0b
jllamas <jllamas@insecami.com.mx>
2020-10-29 15:14:55 -0600

    cambio de formulario

--- radon/users/admin.py
+++ radon/users/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from .forms import UserForm
 
 User = get_user_model()
 
 
-class UserCreateForm(UserCreationForm):
+class UserCreateForm(UserForm):
 
     class Meta:
         model = User
