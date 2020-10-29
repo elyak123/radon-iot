f3236a6694ff07fee212cae43be800730e009b88
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-10-28 20:59:41 -0600

    se anulan las urls temporalmente de gps

--- radon/rutas/urls.py
+++ radon/rutas/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'', views.endpointgps, name='gps'),
-    path('ultima-posicion/', views.last_pos, name='lastpos'),
-    path('mapa/', views.mapa, name='mapa'),
+    # path(r'', views.endpointgps, name='gps'),
+    # path('ultima-posicion/', views.last_pos, name='lastpos'),
+    # path('mapa/', views.mapa, name='mapa'),
 ]
