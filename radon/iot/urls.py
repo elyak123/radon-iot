f380d83889f2fec9d5d7f3b33b52a01601a63223
Javier Llamas Ramirez <elyak.123@gmail.com>
2020-10-28 21:03:43 -0600

    se registra url para registro de lecturas

--- radon/iot/urls.py
+++ radon/iot/urls.py
@@ -1,9 +1,11 @@
+from django.urls import path
 from django.conf import settings
+from radon.iot import views
 
 app_name = 'iot'
 
 urlpatterns = [
-
+    path('sns-lectura-registro/', views.registrolectura, name='sns-lectura-registro'),
 ]
 
 if settings.DEBUG:
