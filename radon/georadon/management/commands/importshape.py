8ef1635ee454ae1b51e97f59e1a9901b06a1d1cc
Javier Llamas Ramirez <jllamas@insecami.com>
2020-12-07 17:15:31 -0600

    commando para importar localidades, falta desacoplar para usarlo en migraciones

--- radon/georadon/management/commands/importshape.py
+++ radon/georadon/management/commands/importshape.py
@@ -1,36 +1,55 @@
+import errno
 import os
 from pathlib import Path
 from collections import OrderedDict
 from django.core.management.base import BaseCommand
 from django.contrib.gis.gdal import DataSource
 from django.contrib.gis.utils import LayerMapping
-from radon.georadon.models import Estado, Municipio
+from radon.georadon.models import Estado, Municipio, Localidad
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        # Asumimos que el archivo deberia de estar en /data/muni_2018gw.shp
-        local_shape = os.path.abspath(os.path.join('docker/production/django/data/muni_2018gw.shp'))
-        production_shape = os.path.abspath(os.path.join('/data/muni_2018gw.shp'))
-        Estado.objects.all().delete()
-        Municipio.objects.all().delete()
+
+    def add_arguments(self, parser):
+        parser.add_argument('municipios', type=str, help='Nombre del shapefile de los municipios sin extensión.')
+        parser.add_argument('localidades', type=str, help='Nombre del shapefile de las localidades sin extensión')
+
+    def get_path(self, shapefile):
+        local_shape = os.path.abspath(os.path.join(f'docker/production/django/data/{shapefile}.shp'))
+        production_shape = os.path.abspath(os.path.join(f'/data/{shapefile}.shp'))
         if Path(local_shape).exists():
-            shape = local_shape
+            return local_shape
         elif Path(production_shape).exists():
-            shape = production_shape
+            return production_shape
+        else:
+            raise FileNotFoundError(
+                errno.ENOENT, os.strerror(errno.ENOENT), shapefile)
+
+    def handle(self, *args, **kwargs):
+        # Asumimos que el archivo deberia de estar en /data/muni_2018gw.shp
+        municipios_shape = self.get_path(kwargs['municipios'])
         mapping = {
             'estado': {'nombre': 'NOM_ENT'},
             'nombre': 'NOM_MUN',
             'clave': 'CVEGEO',
             'geo': 'MULTIPOLYGON'
         }
-        ds = DataSource(str(shape))
+        ds = DataSource(str(municipios_shape))
         # Asumimos que solo es un layer
         layer = ds[0]
         estados = OrderedDict({feat.get('CVE_ENT'): feat.get('NOM_ENT') for feat in layer})
         Estado.objects.bulk_create([Estado(nombre=x[1]) for x in estados.items()])
-        lm = LayerMapping(Municipio, shape, mapping, transform=False)
+        lm = LayerMapping(Municipio, municipios_shape, mapping, transform=False)
         lm.save(strict=True, verbose=True)
+
+        localidades_shape = self.get_path(kwargs['localidades'])
+        mapping_locals = {
+            'nombre': 'NOMBRE',
+            'clave': 'cve_mod',
+            'municipio': {'nombre': 'Municipio'},
+            'geo': 'POLYGON'
+        }
+        loc_ds =  DataSource(str(localidades_shape))
+        lyr = loc_ds[0]
+        lyr_mapping = LayerMapping(Localidad, localidades_shape, mapping_locals, transform=False)
         self.stdout.write(self.style.SUCCESS('Creados los municipios'))
-        if not Path(local_shape).exists() and not Path(production_shape).exists():
-            self.stdout.write('No hay municipios que crear')
