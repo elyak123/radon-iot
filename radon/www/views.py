ea7e0b21cab593f0421ee843c6888104cff3efa5
jllamas <jllamas@insecami.com.mx>
2021-01-22 15:42:14 -0600

    Landing 1era versión

--- radon/www/views.py
+++ radon/www/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = "www/index.html"
+
+
+class PreguntasView(generic.TemplateView):
+    template_name = "www/preguntas.html"
