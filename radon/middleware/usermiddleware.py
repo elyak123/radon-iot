1ff418bb57b338852b937ada859043414b606aed
Javier Llamas Ramirez <elyak.123@gmail.com>
2021-02-01 20:46:33 -0600

    se agrega middleware para tipos de usuario en los requests

---
+++ radon/middleware/usermiddleware.py
@@ -0,0 +1,20 @@
+from django.utils.deprecation import MiddlewareMixin
+from radon.users.models import Consumidor, Operador, Cliente, Staff, SuperUser
+
+
+class UserTypeMappingMiddleware(MiddlewareMixin):
+    user_mapping = {
+        'CONSUMIDOR': Consumidor,
+        'OPERADODR': Operador,
+        'CLIENTE': Cliente
+    }
+
+    def process_request(self, request):
+        if hasattr(request.user, 'tipo'):
+            if request.user.tipo == 'STAFF':
+                if request.user.is_superuser():
+                    request.user.__class__ = SuperUser
+                else:
+                    request.user.__class__ = Staff
+            else:
+                request.user.__class__ = self.user_mapping[request.user.tipo]
