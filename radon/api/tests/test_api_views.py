fefc806e62be53efbc782040f4a246c04c03fe7c
Javier Llamas Ramirez <elyak.123@gmail.com>
2021-02-09 14:34:05 -0600

    tests para auth de api

--- radon/api/tests/test_api_views.py
+++ radon/api/tests/test_api_views.py
@@ -1,6 +1,13 @@
+from django.conf import settings
+from django.http.request import HttpRequest
+from rest_framework.response import Response
+import pytest
+from test_plus.test import CBVTestCase
 from radon.api import views
 from radon.api import serializers
-from test_plus.test import CBVTestCase
+
+HOST = 'api'
+FQN = f'{HOST}.{settings.PARENT_HOST}'
 
 
 def test_APIUsersLoginView_response_serializer(tp):
@@ -9,7 +16,14 @@ def test_APIUsersLoginView_response_serializer(tp):
     assert serializers.ExpirationJWTSerializer == ser
 
 
-# def test_api_activacion_usuarios_calls_users_app_func(tp, mocker):
-#     mocker.patch('radon.api.views.userviews.activacion_usuarios')
-#     view = CBVTestCase.get_instance(views.api_activacion_usuarios)
-    
+@pytest.mark.django_db
+def test_api_activacion_usuarios_calls_users_app_func(tp_api, mocker):
+    mock_view = mocker.patch('radon.api.views.userviews.activacion_usuarios')
+    mock_view.return_value = Response({}, 200, {})
+    mock_permission = mocker.patch('radon.api.views.drf_permissions.IsAuthenticated.has_permission')
+    mock_permission.return_value = True
+    rq = HttpRequest()
+    rq.method = 'POST'
+    views.api_activacion_usuarios(rq)
+    mock_view.assert_called_once()
+    mock_permission.assert_called_once()
