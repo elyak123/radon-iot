0cc22302e993912e6a1ce6af12460fbef43d5133
Javier Llamas Ramirez <elyak.123@gmail.com>
2021-02-09 12:14:41 -0600

    inicio de tests de api

--- radon/api/tests/test_api_views.py
+++ radon/api/tests/test_api_views.py
@@ -1,7 +1,15 @@
 from radon.api import views
+from radon.api import serializers
+from test_plus.test import CBVTestCase
 
 
 def test_APIUsersLoginView_response_serializer(tp):
-    view = tp.get_instance(views.APIUsersLoginView)
+    view = CBVTestCase.get_instance(views.APIUsersLoginView)
     ser = view.get_response_serializer()
-    assert 2 == 1
+    assert serializers.ExpirationJWTSerializer == ser
+
+
+# def test_api_activacion_usuarios_calls_users_app_func(tp, mocker):
+#     mocker.patch('radon.api.views.userviews.activacion_usuarios')
+#     view = CBVTestCase.get_instance(views.api_activacion_usuarios)
+    
